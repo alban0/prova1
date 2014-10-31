eee000e0f8dd0fe216fa7d8704527f0b6b92e3a7
alban <stage03@expertsystem.it>
2014-10-31 13:53:27 +0100

    initial commit

---
+++ Hello/Hello.c
@@ -0,0 +1,15 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+int main(int argc, char* argv)
+{
+	printf("Ciao CIoa");
+	printf("Ciao CIoa");
+
+	
+	
+	system("PAUSE");
+	
+
+
+	return 0;
+}
